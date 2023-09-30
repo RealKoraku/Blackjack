82cff159a1fc772f5bf4d17f9ea48505c8027198
RealKoraku <jakowrach@gmail.com>
2023-09-30 17:53:16 -0500

    Add project files.

---
+++ Blackjack/Card.cs
@@ -0,0 +1,94 @@
+﻿class Card {
+    string _title;
+    string _type;
+    int _value;
+
+    public string Title {
+        get { return _title; }
+        set { _title = value; }
+    }
+
+    public string Type {
+        get { return _type; }
+        set { _type = value; }
+    }
+
+    public int Value {
+        get { return _value; }
+        set { _value = value; }
+    }
+
+    public static List<Card> BuildDeck() {
+
+        List<Card> deck = new List<Card>();
+
+        for (int cardType = 0; cardType < 4; cardType++) {
+            string type = "";
+            if (cardType == 0) {
+                type = "♠";
+            } else if (cardType == 1) {
+                type = "♥";
+            } else if (cardType == 2) {
+                type = "♣";
+            } else if (cardType == 3) {
+                type = "♦";
+            }
+
+            int cardValue = 1;
+
+            for (int cardNum = 1; cardNum < 14; cardNum++) {
+
+                List<string> titles = new List<string>();
+
+                Card currentCard = new Card();
+                currentCard.Type = type;
+                currentCard.Value = cardValue;
+                currentCard.Title = NameCard(type, cardNum);
+
+                deck.Add(currentCard);
+                
+                if (cardType == 1 || cardType == 3) {
+                    PrintRedLn(currentCard.Title);
+                } else {
+                    PrintBlackLn(currentCard.Title);
+                }
+
+                if (cardNum >= 10) {
+                    cardValue = 10;
+                } else {
+                    cardValue++;
+                }
+            }
+        }
+        return deck;
+    }
+
+    public static string NameCard(string type, int cardNum) {
+        string cardName = "";
+
+        if (cardNum == 1) {
+            cardName = $"Ace{type}";
+        } else if (cardNum > 1 && cardNum < 11) {
+            cardName = $"{cardNum}{type}";
+        } else if (cardNum == 11) {
+            cardName = $"Jack{type}";
+        } else if (cardNum == 12) {
+            cardName = $"Queen{type}";
+        } else if (cardNum == 13) {
+            cardName = $"King{type}";
+        }
+        return cardName;
+    }
+
+    public static void PrintBlackLn(string text) {
+        Console.ForegroundColor = ConsoleColor.Black;
+        Console.WriteLine($"{text}");
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+    public static void PrintRedLn(string text) {
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.WriteLine($"{text}");
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
---
+++ Blackjack/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Blackjack {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+
+            Console.WriteLine("BLACKJACK\n");
+            List<Card> deck = Card.BuildDeck();
+        }
+    }
+}
